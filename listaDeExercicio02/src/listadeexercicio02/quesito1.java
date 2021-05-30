a13626a43cddb314b593019c9c904147d338e18c
Gleyson Atanazio <gleysonasilva@gmail.com>
2021-05-30 18:45:26 -0300

    Quesito 1 criado com sucesso

---
+++ listaDeExercicio02/src/listadeexercicio02/quesito1.java
@@ -0,0 +1,32 @@
+/*********************************************************
+ * Ler 3 números fracionários do teclado e informar:     *
+ * se o primeiro é maior do que a soma dos dois últimos; *
+ * Criado por Gleyson Atanazio em 30/05/2021             *
+ *********************************************************
+ */
+package listadeexercicio02;
+
+import java.util.Scanner;
+
+public class quesito1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um numero fracionado. Ex: 1,3. ");
+        double n1 = input.nextDouble();
+        System.out.println("Digite um segundo numero fracionario. ");
+        double n2 = input.nextDouble();
+        System.out.println("Por fim Digite um terceiro número. ");
+        double n3 = input.nextDouble();
+
+        double soma = n2 + n3;
+
+        if (n1 > soma) {
+            System.out.println("Primeiro número maior que a soma dos dois últimos números. ");
+        } else {
+            System.out.println("Primeiro número não é maior que a soma dos dois últimos. ");
+        }
+    }
+
+}
